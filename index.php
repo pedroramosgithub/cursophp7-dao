1b0fe52bd8c3615941fead4146706fe91d298d89
Pedro Ramos <gelatariaportuguesa.castelo@gmail.com>
2023-02-20 17:01:13 +0000

    metodos de lista

--- index.php
+++ index.php
@@ -9,10 +9,30 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios); */
 
 
-$root = new Usuario();
+//$root = new Usuario();
 
-$root->loadById(6);
+//$root->loadById(6);
 
-echo $root; 
+//echo $root; 
+
+
+
+// carrega uma lista de ususarios 
+
+//$lista = usuario::getList();
+//echo json_encode($lista); 
+
+
+// Carrega lista de usuarios procura pelo login 
+
+//$search = usuario::search("jo"); 
+//echo json_encode($search); 
+
+// carrega um ususario usando o login e a senha 
+
+$usuario = new usuario();
+$usuario->login("jose","1234567890"); 
+echo $usuario;
+
 
 ?>
